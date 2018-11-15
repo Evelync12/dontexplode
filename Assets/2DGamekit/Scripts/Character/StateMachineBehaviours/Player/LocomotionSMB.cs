f8de8fa96d27803544a18929463d44f2683f3910
Evelync12 <missionbit@90p-MissionBit.local>
2018-11-14 17:23:32 -0800

    firstupload

---
+++ Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/LocomotionSMB.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Gamekit2D
+{
+    public class LocomotionSMB : SceneLinkedSMB<PlayerCharacter>
+    {
+        public override void OnSLStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.TeleportToColliderBottom();
+        }
+
+        public override void OnSLStateNoTransitionUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            m_MonoBehaviour.UpdateFacing();
+            m_MonoBehaviour.GroundedHorizontalMovement(true);
+            m_MonoBehaviour.GroundedVerticalMovement();
+            m_MonoBehaviour.CheckForCrouching();
+            m_MonoBehaviour.CheckForPushing();
+            m_MonoBehaviour.CheckForHoldingGun();
+            m_MonoBehaviour.CheckAndFireGun ();
+            if (m_MonoBehaviour.CheckForReverseGravity())
+                m_MonoBehaviour.ReverseGravity();
+            if (m_MonoBehaviour.CheckForJumpInput ())
+                m_MonoBehaviour.SetVerticalMovement(m_MonoBehaviour.jumpSpeed);
+            else if(m_MonoBehaviour.CheckForMeleeAttackInput ())
+                m_MonoBehaviour.MeleeAttack();
+        }
+    }
+}
---
+++ Assets/2DGamekit/Scripts/ExampleCharacters/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+    public float maxSpeed = 10;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	private void FixedUpdate () {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, Input.GetAxis("Vertical") * maxSpeed);
+        animator.SetBool("moving", (rb.velocity.x != 0 || rb.velocity.y != 0));
+	}
+}
